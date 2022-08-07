0fb604be625953d3000c8c2cfaecab7783941c38
Phạm Quang Đức <duc.pq194518@sis.hust.edu.vn>
2022-08-07 11:24:49 +0700

    Buoi3

---
+++ buoi3/app/src/main/java/com/example/buoi3/recyclerView/RecyclerViewActivity2.java
@@ -0,0 +1,16 @@
+package com.example.buoi3.recyclerView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.buoi3.R;
+
+public class RecyclerViewActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view2);
+    }
+}
---
+++ buoi3/app/src/main/java/com/example/buoi3/recyclerView/studentadapter.java
@@ -0,0 +1,8 @@
+package com.example.buoi3.recyclerView;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
